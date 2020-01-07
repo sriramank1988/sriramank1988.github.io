f80d10f02a42529c7ad9a3a4761c076c29c96cdf
Sriram Krishnan <sriramank@live.in>
2020-01-07 15:23:25 +1100

    Player marker and Layout is completed

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Linked Properly")
+var playerTurn = document.querySelector('body h3 span').textContent;
+var allBoxes = document.querySelectorAll('.box');
+allBoxes.forEach( function(item){
+    item.addEventListener('click',function(event){
+        if(event.target.textContent === ""){ //Ensure Text content is Empty
+            if(playerTurn === 'X'){
+                event.target.textContent = playerTurn;
+                playerTurn = 'O';
+                document.querySelector('body h3 span').textContent = 'O'
+                console.log(playerTurn)                
+            }
+            else{
+                event.target.textContent = playerTurn;
+                playerTurn = 'X';
+                document.querySelector('body h3 span').textContent = 'X';
+                console.log(playerTurn)
+            }
+        }
+    })
+})
+
